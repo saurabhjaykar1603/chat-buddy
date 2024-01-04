09ce4f09a8d1781132a850ab308d68725471f04e
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-01-04 11:50:09 +0530

    chore : define user schema and model

---
+++ server/model/User.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const newUserSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", newUserSchema);
+export default User;
