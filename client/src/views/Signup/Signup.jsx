5203a595c4a891e2e148ddbe8b5c4d12c8432c0d
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-01-14 18:47:40 +0530

    style : changes in Signup component

--- client/src/views/Signup/Signup.jsx
+++ client/src/views/Signup/Signup.jsx
@@ -9,52 +9,52 @@ function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
- const SignupUser = async () => {
-  if (!userName) {
-    showToast("User name is required", "warning", 4000);
-    return;
-  }
-  if (!email) {
-    showToast("Email is required", "warning", 4000);
-    return;
-  }
-  if (!password) {
-    showToast("Password is required", "warning", 4000);
-    return;
-  }
-
-  try {
-    const res = await axios.post("/api/v1/signups", {
-      userName,
-      email,
-      password,
-    });
-
-    if (res.data.message === "Name already exists") {
-      return showToast("User name already exists", "error", 4000);
+  const SignupUser = async () => {
+    if (!userName) {
+      showToast("User name is required", "warning", 4000);
+      return;
     }
-    if (res.data.message === "Email already Exits") {
-      return showToast("Email already exists", "error", 4000);
+    if (!email) {
+      showToast("Email is required", "warning", 4000);
+      return;
+    }
+    if (!password) {
+      showToast("Password is required", "warning", 4000);
+      return;
     }
 
-    if (res.data.success) {
-      alert(res.data.message);
-      window.location.href = "/login";
-    } else {
-      alert(res.data.message);
-    }
-  } catch (err) {
-    showToast("username & email already exist", "error", 4000);
-  }
-};
+    try {
+      const res = await axios.post("/api/v1/signups", {
+        userName,
+        email,
+        password,
+      });
 
+      if (res.data.message === "Name already exists") {
+        return showToast("User name already exists", "error", 4000);
+      }
+      if (res.data.message === "Email already Exits") {
+        return showToast("Email already exists", "error", 4000);
+      }
+
+      if (res.data.success) {
+        alert(res.data.message);
+        window.location.href = "/login";
+      } else {
+        alert(res.data.message);
+      }
+    } catch (err) {
+      showToast("username & email already exist", "error", 4000);
+    }
+  };
 
   return (
     <div className="h-screen bg-slate-900 overflow-hidden  ">
       <form className="form mx-auto mt-36">
         <p className="form-title">Sign in to your account</p>
-        <div className="input-container">
+        <div className="input-container ">
           <input
+            className="w-full"
             value={userName}
             type="text"
             placeholder="enter user name"
@@ -64,6 +64,7 @@ function Signup() {
         </div>
         <div className="input-container">
           <input
+            className="w-full"
             value={email}
             type="email"
             placeholder="enter email"
@@ -73,6 +74,7 @@ function Signup() {
         </div>
         <div className="input-container">
           <input
+            className="w-full"
             type="password"
             placeholder="Enter password"
             onChange={(e) => setPassword(e.target.value)}
