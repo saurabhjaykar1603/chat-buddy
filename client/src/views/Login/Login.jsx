d8a71c95dca9f46053be39b65ccd3557a51349f2
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-01-14 19:12:51 +0530

    feat : login api integrated successfully

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -1,9 +1,72 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import showToast from "crunchy-toast";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const SignupUser = async () => {
+    if (!email) {
+      showToast("Email is required", "warning", 4000);
+      return;
+    }
+    if (!password) {
+      showToast("Password is required", "warning", 4000);
+      return;
+    }
+  
+    try {
+      const response = await axios.post("/api/v1/logins", { email, password });
+      if (response?.data?.success) {
+        localStorage.setItem("user", JSON.stringify(response?.data?.data));
+        window.location.href = "/";
+      } else {
+        showToast(response?.data?.message || "An error occurred", "warning", 4000);
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        showToast("Unauthorized: Invalid Credential", "warning", 4000);
+      } else {
+        console.log(error);
+      }
+    }
+  };
+  
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="h-screen bg-slate-900 overflow-hidden  ">
+      <form className="form mx-auto mt-36">
+        <p className="form-title">Login in to your account</p>
+
+        <div className="input-container">
+          <input
+            className="w-full"
+            value={email}
+            type="email"
+            placeholder="enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <span></span>
+        </div>
+        <div className="input-container">
+          <input
+            className="w-full"
+            type="password"
+            placeholder="Enter password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button onClick={SignupUser} type="button" className="submit">
+          Sign in
+        </button>
+
+        <p className="signup-link">
+          Already have an account?
+          <Link to="/login">login</Link>
+        </p>
+      </form>
     </div>
   );
 }
